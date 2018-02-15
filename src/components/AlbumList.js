c2029ce3513a1d53d90a0841541436290eb34d72
Gaurav <gaurav.dwivedi1990@gmail.com>
2018-02-16 00:06:29 +0530

    Card style component with api hit

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
 class AlbumList extends Component {
+    state = { albums:[]}
+    // AlbumList.state: {
+    //     [x: string]: any;
+    //     albums: any[];
+    // }
+    componentWillMount(){
+            axios.get('http://postalpincode.in/api/pincode/400072')
+            .then(response => this.setState({albums:response.data.PostOffice}))
+    }
+    renderAlbumName(){
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.Name} album={album} />
+        // <Text key={album.Name}>{album.Name}</Text>
+    )
+    }
     render() {
+        console.log(this.state.albums)
         return (
             <View>
-                <Text>AlbumList</Text>
+
+                {this.renderAlbumName()/* <Text>AlbumList</Text> */}
             </View>
         );
     }
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+
+import React from 'react'
+import {View} from 'react-native'
+
+const Card = (props) => {
+return (
+    <View style={styles.containerStyle}>
+        {/* <Text>{props.album.Name}</Text> */}
+        {props.children}
+    </View>
+)
+}
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius:2,
+        boderColor: '#ddd',
+        borderBottomWidth:0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        elevation: 1,
+        marginLeft:5,
+        marginRight:5,
+        marginTop:10
+
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+export default Card;
